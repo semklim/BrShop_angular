1bbcfdccb1f1bcbd50c691c08e232ebe72212541
Roman Klymashenko <romanklimashenko@gmail.com>
2023-05-23 21:36:47 +0300

    add Eslint, styleguide AirBnB, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+};
