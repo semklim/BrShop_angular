18eb97588d2686d78a4731707430007cf4a48d30
DorianNet93 <lifebsfg@gmail.com>
2023-06-06 08:27:36 +0300

    DecorMainPage_StartGetCompSize_StartAdminPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('navigation', { static: false }) navigation!: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.navigation.nativeElement);
+  }
+}
