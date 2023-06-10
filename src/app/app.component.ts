00e2bcc333543794d8bfcf37356edc9672a1c9e9
Roman Klymashenko <romanklimashenko@gmail.com>
2023-06-10 19:50:56 +0300

    replace searchBar into main component. dell examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,8 @@
 import { Component } from '@angular/core';
-import { ImgurService } from './services/imgur.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private imgur: ImgurService) {}
-
-  // 1
-  async uploadImgByLocalUrl() {
-    const res = await this.imgur.uploadImg('assets/air-jordan-xxxvii-prm-4.png');
-    console.log(res);
-  }
-
-  // 2
-  async uploadImgByUrl(input: HTMLInputElement) {
-    const res = await this.imgur.uploadImg(input.value);
-    console.log(res);
-  }
-
-  // 3
-  async uploadImgByFile(input: HTMLInputElement) {
-    if (input.files) {
-      const res = await this.imgur.uploadImg(input.files[0]);
-      console.log(res);
-    }
-  }
-}
+export class AppComponent {}
