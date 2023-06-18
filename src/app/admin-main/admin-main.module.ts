22ec61953d18b6cc7c5df10b66ecdf831a504b0a
DorianNet93 <lifebsfg@gmail.com>
2023-06-19 00:21:05 +0300

    AdminForm_1.0

--- src/app/admin-main/admin-main.module.ts
+++ src/app/admin-main/admin-main.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AdminMainComponent } from './admin-main.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AdminMainComponent],
-  imports: [RouterModule.forChild([{ path: '', component: AdminMainComponent }]), FormsModule, CommonModule],
+  imports: [
+    RouterModule.forChild([{ path: '', component: AdminMainComponent }]),
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AdminMainModule {}
