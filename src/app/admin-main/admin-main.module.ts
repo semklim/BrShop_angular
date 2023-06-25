b48f6fd23764b8af6be193423969f01d4a5b7a14
Roman Klymashenko <romanklimashenko@gmail.com>
2023-06-25 22:59:39 +0300

    all UI modules replace to lib folder

--- src/app/admin-main/admin-main.module.ts
+++ src/app/admin-main/admin-main.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FBaseService } from '../services/fireStore/fbase.service';
-import { SearchModule } from '../searchBar/search.module';
+import { SearchModule } from 'src/libs/ui/searchBar/search.module';
 
 @NgModule({
   declarations: [AdminMainComponent],
--- src/app/mainPage/main-page.module.ts
+++ src/app/mainPage/main-page.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { CardComponent } from './productCard/card.component';
-import { ImageSliderModule } from '../imageSlider/imageSlider.module';
 import { FormsModule } from '@angular/forms';
-import { SearchModule } from '../searchBar/search.module';
+import { SearchModule } from 'src/libs/ui/searchBar/search.module';
+import { ImageSliderModule } from 'src/libs/ui/imageSlider/imageSlider.module';
+import { CardComponent } from 'src/libs/ui/productCard/card.component';
 
 @NgModule({
   declarations: [MainPageComponent, CardComponent],
