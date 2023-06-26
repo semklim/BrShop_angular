ef1082187357a819b344990604e2a27bc626f45c
Roman Klymashenko <romanklimashenko@gmail.com>
2023-06-26 17:39:58 +0300

    add review block and logic

--- src/libs/ui/review/review.component.ts
+++ src/libs/ui/review/review.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/types/products';
+import { Review } from 'src/app/types/review';
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css'],
 })
-export class ReviewComponent {}
+export class ReviewComponent {
+  @Input() reviewData?: Review[];
+
+  @Output() reviewChange: EventEmitter<Review> = new EventEmitter();
+
+  review: Review = {
+    rating: 0,
+    review_id: '',
+    comment: '',
+    username: '',
+  };
+
+  reviewForm: FormGroup;
+
+  constructor(private buildForm: FormBuilder) {
+    this.reviewForm = buildForm.group({
+      username: buildForm.control('', [Validators.minLength(3), Validators.required]),
+      rating: buildForm.control('5', [Validators.minLength(3), Validators.required]),
+      comment: buildForm.control('', [Validators.minLength(6), Validators.required]),
+    });
+  }
+
+  submit() {
+    console.log(this.reviewForm.value);
+    this.reviewChange.emit(this.reviewForm.value);
+    this.reviewForm.reset();
+  }
+}
--- src/libs/ui/review/review.module.ts
+++ src/libs/ui/review/review.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReviewComponent } from './review.component';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { StarRatingModule } from 'angular-star-rating';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ReviewComponent],
-  imports: [CommonModule, StarRatingModule.forRoot()],
+  imports: [CommonModule, ReactiveFormsModule, StarRatingModule.forRoot()],
   exports: [ReviewComponent],
 })
 export class ReviewModule {}
