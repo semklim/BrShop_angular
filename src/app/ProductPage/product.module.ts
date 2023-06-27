b35e4f5080eb047bb5eb46b91464e79b0ed580b1
Nikita <egorovnikita2002@gmail.com>
2023-06-27 17:48:08 +0200

    rating added

--- src/app/ProductPage/product.module.ts
+++ src/app/ProductPage/product.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { RouterModule } from '@angular/router';
 import { ReviewModule } from 'src/libs/ui/review/review.module';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   declarations: [ProductComponent],
-  imports: [CommonModule, RouterModule.forChild([{ path: '', component: ProductComponent }]), ReviewModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: ProductComponent }]),
+    ReviewModule,
+    StarRatingModule.forRoot(),
+  ],
 })
 export class ProductModule {}
