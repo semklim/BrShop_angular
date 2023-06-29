7ea0dc8e30b480c2bb24da7df3fb97c0b951d5a2
Roman Klymashenko <romanklimashenko@gmail.com>
2023-06-29 23:39:04 +0300

    hot fix + add ignore zip

--- src/app/admin-main/admin-main.module.ts
+++ src/app/admin-main/admin-main.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FBaseService } from '../services/fireStore/fbase.service';
 import { SearchModule } from 'src/libs/ui/searchBar/search.module';
+import { EllipsePipe } from './pipe ellipse/ellipse.pipe';
 
 @NgModule({
-  declarations: [AdminMainComponent],
+  declarations: [AdminMainComponent, EllipsePipe],
   imports: [
     RouterModule.forChild([{ path: '', component: AdminMainComponent }]),
     FormsModule,
