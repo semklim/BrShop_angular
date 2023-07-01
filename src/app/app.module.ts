4c699cd8648fa702ba12d587e2e27c3d01125256
Roman Klymashenko <romanklimashenko@gmail.com>
2023-07-01 22:28:11 +0300

    cleanUp code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { HttpClientModule } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-// import { AngularFireModule } from '@angular/fire/compat';
 import { MainPageModule } from './mainPage/main-page.module';
-import { FBaseService } from './services/fireStore/fbase.service';
-import { LocalDataService } from './services/localDataBase/localData.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
 @NgModule({
@@ -21,16 +17,13 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     FormsModule,
-    // AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
     MainPageModule,
   ],
-  providers: [{ provide: FBaseService, useClass: environment.production ? FBaseService : LocalDataService }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
