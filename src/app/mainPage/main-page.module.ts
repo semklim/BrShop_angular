d9a5779bced9faf1caa369562b23a8d15eff75e7
Roman Klymashenko <romanklimashenko@gmail.com>
2023-07-06 22:53:40 +0300

    delete not used import

--- src/app/mainPage/main-page.module.ts
+++ src/app/mainPage/main-page.module.ts
@@ -4,20 +4,11 @@ import { MainPageComponent } from './main-page.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { SearchModule } from 'src/libs/ui/searchBar/search.module';
-import { ImageSliderModule } from 'src/libs/ui/imageSlider/imageSlider.module';
 import { LoadingModule } from 'src/libs/ui/loading/loading.module';
 import { ProductCardModule } from 'src/libs/ui/productCard/card.module';
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [
-    CommonModule,
-    LoadingModule,
-    ProductCardModule,
-    AppRoutingModule,
-    ImageSliderModule,
-    SearchModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, LoadingModule, ProductCardModule, AppRoutingModule, SearchModule, FormsModule],
   exports: [MainPageComponent],
 })
 export class MainPageModule {}
