954c766d9cc2975d930971b91c6ebf49873272eb
Roman Klymashenko <romanklimashenko@gmail.com>
2023-07-28 18:25:34 +0300

    del excessive import

--- src/app/mainPage/main-page.module.ts
+++ src/app/mainPage/main-page.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { SearchModule } from 'src/libs/ui/searchBar/search.module';
 import { LoadingModule } from 'src/libs/ui/loading/loading.module';
 import { ProductCardModule } from 'src/libs/ui/productCard/card.module';
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [CommonModule, LoadingModule, ProductCardModule, AppRoutingModule, SearchModule, FormsModule],
+  imports: [CommonModule, LoadingModule, ProductCardModule, SearchModule, FormsModule],
   exports: [MainPageComponent],
 })
 export class MainPageModule {}
