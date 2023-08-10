91cc30cdfe69192a4444e573a8e5663fb78eebf4
Roman Klymashenko <romanklimashenko@gmail.com>
2023-08-10 11:32:47 +0300

    change api for IpInfo

--- src/app/services/checkIp/check-ip.service.ts
+++ src/app/services/checkIp/check-ip.service.ts
@@ -1,65 +1,38 @@
 import { Injectable } from '@angular/core';
 
-export type CheckIpResponse = {
-  ipNumeric: number;
-  isBehindProxy: boolean;
-  ipVersion: number;
-  ipAddress: string;
-  latitude: number;
-  longitude: number;
-  countryName: string;
-  countryCode: string;
-  timeZone: string;
-  zipCode: string;
-  cityName: string;
-  regionName: string;
-  continent: string;
-  continentCode: string;
-};
-
-type ApiBdcResponse = {
-  ipString: string;
-  ipNumeric: number;
-  ipType: string;
-  isBehindProxy: boolean;
-};
-
-type FreeIpApiResponse = {
-  ipVersion: number;
-  ipAddress: string;
-  latitude: number;
-  longitude: number;
-  countryName: string;
-  countryCode: string;
-  timeZone: string;
-  zipCode: string;
-  cityName: string;
-  regionName: string;
-  continent: string;
-  continentCode: string;
+export type IpInfoResponse = {
+  ip: string;
+  hostname: string;
+  city: string;
+  region: string;
+  country: string;
+  loc: string;
+  org: string;
+  postal: string;
+  timezone: string;
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckIpService {
-  ipData: Promise<CheckIpResponse>;
+  ipData: Promise<IpInfoResponse>;
 
   constructor() {
     this.ipData = this.checkIp();
   }
 
   async checkIp() {
-    const ipInfo = await fetch('https://api-bdc.net/data/client-ip').then(
-      (response) => response.json() as unknown as ApiBdcResponse,
-    );
-    const url = `https://freeipapi.com/api/json/${ipInfo.ipString}`;
-    const moreIpData = await fetch(url).then((response) => response.json() as unknown as FreeIpApiResponse);
-    const result: CheckIpResponse = {
-      ...moreIpData,
-      isBehindProxy: ipInfo.isBehindProxy,
-      ipNumeric: ipInfo.ipNumeric,
-    };
-    return result;
+    // const ipInfo = await fetch('https://api-bdc.net/data/client-ip').then(
+    //   (response) => response.json() as unknown as ApiBdcResponse,
+    // );
+    const url2 = `https://ipinfo.io/json?token=240fec7368fc4c`; // this is better.
+    try {
+      const moreIpData = await fetch(url2).then((response) => response.json() as unknown as IpInfoResponse);
+      return moreIpData;
+    } catch (e: any) {
+      console.log(e.message);
+      return { error: `Can't fetch data` } as unknown as IpInfoResponse;
+    }
   }
 }
